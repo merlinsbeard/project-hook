cc0d3d0f899b764b45880f4298c9e790e338aad0
BJ Paat <brpaat@ayannah.com>
2017-06-23 19:01:39 +0800

    Update model for prompt
    Removed Author field in Prompt.
    
    Add prompt comment url.
    
    Change comment field to story for clarity.

--- prompts/models.py
+++ prompts/models.py
@@ -1,21 +1,28 @@
 from django.db import models
+from django.urls import reverse
 
 
 class Prompt(models.Model):
     title = models.CharField(max_length=255)
     link = models.URLField()
-    author = models.CharField(max_length=255)
     slug = models.SlugField(max_length=255, unique=True)
 
     def __str__(self):
         return self.title
 
+    def get_absolute_url(self):
+        return reverse('prompts:detail', kwargs={'slug': self.slug})
+
 
 class PromptComment(models.Model):
     title = models.ForeignKey(Prompt)
-    slug = models.SlugField(unique=True)
-    comment = models.TextField()
     author = models.CharField(max_length=255)
+    slug = models.SlugField(unique=True)
+    story = models.TextField()
 
     def __str__(self):
         return self.author
+
+    def prompt_url(self):
+        url = self.title.link + self.slug
+        return url
